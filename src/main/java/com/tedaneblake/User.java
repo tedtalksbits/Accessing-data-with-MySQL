c15d1502a620cbf6cde3ee606a1e096991d52c4d
tedtalksbits <tedaneblake@gmail.com>
2022-04-28 22:08:06 -0400

    feat: Create @Entity model

---
+++ src/main/java/com/tedaneblake/User.java
@@ -0,0 +1,40 @@
+package com.tedaneblake;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private String name;
+	
+	private String email;
+	
+	public Integer getId() {
+		return id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
